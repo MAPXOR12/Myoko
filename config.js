5d0b1ad686c839af583293901c3090d76d35ee75
Devil Dev <84636715+MAPXOR12@users.noreply.github.com>
2022-12-26 19:50:50 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  token:process.env.token,
+  token: "Nzg0MzA0Mzg1NTcxOTQ2NTA2.G2Ton6.W9nwWEjm5qWoe8GeP3eYmlBA7XrEKF1bXOOnFI",
   globalBot:false, // false => if bot is private bot - true => if bot isn't private bot
   guildID:"Server ID",
 }
